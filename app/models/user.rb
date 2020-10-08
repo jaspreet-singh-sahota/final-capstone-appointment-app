5ca1488d2e2bfaed84c71e52d6333e4189eb7d28
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-09 05:06:36 +0530

    Add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
+  VALID_EMAIL_ID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :name, presence: true,
+   length: { minimum: 3, maximum: 30 }
+  validates :email, presence: true,
+   length: { minimum: 10, maixmum: 80 },
+    format: { with: VALID_EMAIL_ID }
 end
+
