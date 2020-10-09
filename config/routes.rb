8771bb9a79559bae1c6a0a947de26896730483ff
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-09 06:37:28 +0530

    Add login and logout routes in sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   root 'homepage#index'
   resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
 end
