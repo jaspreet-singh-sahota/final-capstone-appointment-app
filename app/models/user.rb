a96395815aac892c5494cd2f60ba066827353568
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-09 06:02:30 +0530

    Add session controller with create method

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   has_secure_password
 
   VALID_EMAIL_ID = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
-  validates :name, presence: true,
+  validates :username, presence: true, uniqueness: true,
    length: { minimum: 3, maximum: 30 }
-  validates :email, presence: true,
+  validates :email, presence: true, uniqueness: true,
    length: { minimum: 10, maixmum: 80 },
     format: { with: VALID_EMAIL_ID }
 end
