5dfda5b66790a687420f2d07f3a482e916316c5e
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-10 04:56:19 +0530

    Add validations to facitity model

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,2 +1,12 @@
 class Facility < ApplicationRecord
+
+  validates :name, presence: true, uniqueness: true,
+   length: { minimum: 2, maximum: 30 }
+  validates :image_url, presence: true, uniqueness: true
+  validates :brief_description, presence: true
+  validates :detailed_description, presence: true
+  validates :free_trial, presence: true
+  validates :three_months_package, presence: true
+  validates :six_months_package, presence: true
+  validates :annual_package, presence: true
 end
