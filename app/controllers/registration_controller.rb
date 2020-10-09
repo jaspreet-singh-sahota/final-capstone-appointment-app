fdea19494b93aa11948bb86fc32112fd34305b62
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-09 06:22:41 +0530

    Add RegistrationsController with create method

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::RegistrationsController < ApplicationController
+  def create
+    user = User.new(username: params['user']['username']
+                    email: params['user']['email'],
+                    password: params['user']['password'],
+                    password_confirmation: params['user']['password_confirmation'])
+
+    if user.save
+      session[:user_id] = user.id
+      render json: { status: :created, user: user }
+    else
+      render json: { errors: user.errors.full_messages }
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   root 'homepage#index'
+  resources :registrations, only: [:create]
 end
