6dbfb7ffa8c1fc1effd2046407d46f581a14252b
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-10 08:22:56 +0530

    Add facilities get route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions, only: [:create]
   root 'homepage#index'
   resources :registrations, only: [:create]
+  resources :facilities, only: [:index]
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
 end
