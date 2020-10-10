355dfff922b371581ecbb9eb7885ee52bd38ea7c
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-10 08:37:04 +0530

    Add appointment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_230234) do
+ActiveRecord::Schema.define(version: 2020_10_10_030405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "appointments", force: :cascade do |t|
+    t.string "date"
+    t.string "city"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "facilities", force: :cascade do |t|
     t.string "name"
