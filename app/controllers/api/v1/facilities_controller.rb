02da4ca40803a4fcc9b4ab0884f9492ed2825f71
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-17 00:29:38 +0530

    Fix heroku API routing error

--- app/controllers/api/v1/facilities_controller.rb
+++ app/controllers/api/v1/facilities_controller.rb
@@ -1,4 +1,4 @@
-class   Api::V1::FacilitiesController < ApplicationController
+class Api::V1::FacilitiesController < ApplicationController
   def index
     facilities = Facility.all
     render json: facilities
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api, :defaults => {:format => :json} do
+  namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
       resources :registrations, only: [:create]
