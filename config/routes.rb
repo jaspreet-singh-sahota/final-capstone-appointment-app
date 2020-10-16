1054abfe1c07839ad31667cab834781b7dad1476
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-17 01:24:15 +0530

    Finally heroku error fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :facilities, only: [:index]
   namespace :api do
     namespace :v1 do
       resources :sessions, only: [:create]
       resources :registrations, only: [:create]
+      resources :facilities, only: [:index]
       delete :logout, to: 'sessions#logout'
       get :logged_in, to: 'sessions#logged_in'
       post :set_appointment, to: 'appointments#create'
