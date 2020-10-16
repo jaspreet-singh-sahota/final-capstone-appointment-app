1abff1b380a8a6f34dac87b1f4911844180dcb2d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-16 17:44:41 +0530

    Fix routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'homepage#index'
   resources :registrations, only: [:create]
   resources :facilities, only: [:index]
-  resources :appointments,  only: [:index, :create, :destroy]
   delete :logout, to: 'sessions#logout'
   get :logged_in, to: 'sessions#logged_in'
+  post :set_appointment, to: 'appointments#create'
+  get :get_appointment, to: 'appointments#index'
 end
