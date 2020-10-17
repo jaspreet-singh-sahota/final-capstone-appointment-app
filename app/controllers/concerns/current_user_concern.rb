46f5deccfa947482e4f330c6ff6ab098a5274a42
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-17 13:32:14 +0530

    Update production link

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,6 +7,6 @@ module CurrentUserConcern
 
   def set_current_user
     @current_user ||= session[:user_id] &&
-      User.find_by(id: session[:user_id])
+      User.find(session[:user_id])
   end
 end
