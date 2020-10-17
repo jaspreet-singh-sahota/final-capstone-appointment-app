f48658d732cabb4800950ceabac93f138c12e399
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-17 12:39:15 +0530

    Fix session error in production

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -6,8 +6,7 @@ module CurrentUserConcern
   end
 
   def set_current_user
-    return @current_user if @current_user
-
-    @current_user = User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= session[:user_id] &&
+      User.find_by(id: session[:user_id])
   end
 end
