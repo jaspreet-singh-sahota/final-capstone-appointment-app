15fd5bb2ce42d07c7059e10900c8a1d2238d30d1
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-18 16:16:34 +0530

    Add facilities factory-bot

---
+++ spec/factories/facilities.rb
@@ -0,0 +1,13 @@
+# spec/factories/todos.rb
+FactoryBot.define do
+  factory :facility do
+    name {  Faker::Name.unique.first_name }
+    sequence(:image_url) { |i| "someURL#{i}" }
+    sequence(:brief_description) { |i| "sometext#{i}" }
+    sequence(:detailed_description) { |i| "sometext#{i}" }
+    free_trial { "free trail" }
+    three_months_package { "100$" }
+    six_months_package { '150$' }
+    annual_package { '250$' }
+  end
+end
