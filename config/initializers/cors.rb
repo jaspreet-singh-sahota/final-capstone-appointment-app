8685054ddb2c3271638187bf19a99a94fa3088ac
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 03:32:29 +0530

    Fix cors bug again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,15 +1,9 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://jassi-cultfit-app.herokuapp.com/'
-
-    resource '/cors',
-      :headers => :any,
-      :methods => [:post],
-      :max_age => 0
+    origins ['http://localhost:3000', 'https://jassi-cultfit-app.herokuapp.com/']
 
     resource '*',
-      :headers => :any,
-      :methods => [:get, :post, :delete, :put, :patch, :delete, :options, :head],
-      :max_age => 0
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
