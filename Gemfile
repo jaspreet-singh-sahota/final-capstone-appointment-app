fe1a48233a7bc306ad1a9e26eaf7de90b1dd978d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-18 14:04:29 +0530

    Add tests for facility model

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ group :development do
   # Access an interactive console on exception pages or by calling 'console' anywhere in the code.
   gem 'web-console', '>= 3.3.0'
   gem 'listen', '~> 3.2'
+  gem 'rspec-rails', '~> 3.5'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
@@ -49,6 +50,10 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of web drivers to run system tests with browsers
   gem 'webdrivers'
+  gem 'factory_bot_rails', '~> 4.0'
+  gem 'shoulda-matchers', '~> 3.1'
+  gem 'faker'
+  gem 'database_cleaner'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -4,11 +4,12 @@ class Facility < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true,
    length: { minimum: 2, maximum: 30 }
-  validates :image_url, presence: true, uniqueness: true
-  validates :brief_description, presence: true
-  validates :detailed_description, presence: true
-  validates :free_trial, presence: true
-  validates :three_months_package, presence: true
-  validates :six_months_package, presence: true
-  validates :annual_package, presence: true
+  validates :image_url, presence: true, uniqueness: true,
+    length: { minimum: 2}
+  validates :brief_description, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :detailed_description, presence: true, uniqueness: true, length: { minimum: 2 }
+  validates :free_trial, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :three_months_package, presence: true, length: { minimum: 2, maximum: 20}
+  validates :six_months_package, presence: true, length: { minimum: 2, maximum: 20}
+  validates :annual_package, presence: true, length: { minimum: 2, maximum: 20 }
 end
