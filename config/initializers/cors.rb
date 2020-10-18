5175111bed47fa0d0dfebe137b7bbb9a4cf411fa
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 04:19:06 +0530

    Update cors origins link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins '*'
+    origins 'https://jassi-cultfit-app.herokuapp.com/'
 
     resource '/cors',
       :headers => :any,
