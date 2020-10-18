a6e206802656a3086a05bc81bfc0b0f6d9d123c9
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-18 23:59:38 +0530

    Update heroku link

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   end
 
   allow do
-    origins "https://jassi-appointment-app.herokuapp.com/"
+    origins "https://jassi-cultfit-app.herokuapp.com/"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 end
