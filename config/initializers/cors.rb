05fe3e2113484b3671cacde276c880da64f9095d
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-10-19 11:14:07 +0530

    Add multiple origins to cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: true, logger: (-> { Rails.logger }) do
   allow do
-    origins 'https://jassi-cultfit-app.herokuapp.com/'
-
-    resource '/cors',
-      :headers => :any,
-      :methods => [:post],
-      :max_age => 0
+    origins ['https://jassi-cultfit-app.herokuapp.com/',
+      'https://jassi-cultfit-app.herokuapp.com/appointments',
+      'https://jassi-cultfit-app.herokuapp.com/login',
+      'https://jassi-cultfit-app.herokuapp.com/sign-in',  ]
 
     resource '*',
       :headers => :any,
